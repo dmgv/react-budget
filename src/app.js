75abb85a6e22a2fa44caf0e4fff3b3fe92ad765e
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-04-15 17:42:28 -0300

    Criar o router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const DashboardPage = () => <Fragment><h1>Dashboard</h1></Fragment>;
+
+const AddExpensePage = () => <Fragment><h1>Add</h1></Fragment>;
+
+const EditExpensePage = () => <Fragment><h1>Edit</h1></Fragment>;
+
+const HelpPage = () => <Fragment><h1>Help</h1></Fragment>;
+
+const routes = (
+  <BrowserRouter>
+    <Fragment>
+      <Route path="/" component={DashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} exact={true} />
+      <Route path="/edit" component={EditExpensePage} exact={true} />
+      <Route path="/help" component={HelpPage} exact={true} />
+    </Fragment>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
