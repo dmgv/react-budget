a1ff3f1dc270f6b6cb7c8d992657b3e28be7abe8
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-04-16 15:17:59 -0300

    Refactoring with destruction

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,12 +1,29 @@
 import { createStore } from "redux";
 
+console.log();
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: "INCREMENT",
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: "DECREMENT",
+  decrementBy
+});
+
+const setCount = ({ count }) => ({
+  type: "SET",
+  count
+});
+
+const resetCount = () => ({ type: "RESET" });
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      const incrementBy =
-        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case "DECREMENT":
       const decrementBy =
@@ -31,29 +48,14 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: "INCREMENT",
-  incrementBy: 5
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-  type: "INCREMENT"
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: "RESET"
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: "DECREMENT"
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: "DECREMENT",
-  decrementBy: 10
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-  type: "SET",
-  count: 101
-});
+store.dispatch(setCount({ count: 101 }));
