e171ee919c48c226f2035dbd0524cb3de1f8a995
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-04-22 14:59:17 -0300

    Add sorty by selector

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { connect } from "react-redux";
-import { setTextFilter } from "../actions/filters";
+import { setTextFilter, sortByDate, sortByAmount } from "../actions/filters";
 
 const ExpenseListFilters = props => (
   <Fragment>
@@ -11,6 +11,20 @@ const ExpenseListFilters = props => (
         props.dispatch(setTextFilter(e.target.value));
       }}
     />
+
+    <select
+      value={props.filters.sortyBy}
+      onChange={(e) => {
+        if(e.target.value === 'date') {
+          props.dispatch(sortByDate())
+        } else if (e.target.value === 'amount'){
+          props.dispatch(sortByAmount())
+        }
+      }}
+    >
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
   </Fragment>
 );
 
