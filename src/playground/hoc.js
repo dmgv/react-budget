49eaec3ca36a99e2cc17eab34f5521b2b8be04d7
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-04-22 09:30:24 -0300

    Add HOC exemple

---
+++ src/playground/hoc.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+const Info = props => (
+  <Fragment>
+    <h1>Info</h1>
+    <p>The info is:<i>{props.info}</i></p>
+  </Fragment>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <Fragment>
+      {props.isAdmin && <p>Private! Do not share</p>}
+      <WrappedComponent {...props}/>
+    </Fragment>
+  )
+}
+
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <Fragment>
+      {props.isAuthenticated ? <WrappedComponent {...props} /> : <p>Tem que logar primerio</p>}
+    </Fragment>
+  )
+}
+
+const AuthInfo = requireAuthentication(Info);
+
+const AdminInfo =  withAdminWarning(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="There are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={false} info="There are the details" />, document.getElementById('app'));
