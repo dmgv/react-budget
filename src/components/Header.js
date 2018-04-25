b8d0abe444473634fef45dcce23caa35d832a413
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-04-25 17:17:29 -0300

    Install and configure enzyme

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 const Header = () => (
   <Fragment>
     <header>
-      <h1>Expenify</h1>
+      <h1>Expensify</h1>
       <nav>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Add</NavLink>
