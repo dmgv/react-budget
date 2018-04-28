0c14031e1b0fdbb995f746c30f92fec02676b82d
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-04-28 15:02:42 -0300

    Add formatting for amount and date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React, {Fragment} from "react";
 import { Link } from "react-router-dom";
+import moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <Fragment>
@@ -7,7 +9,9 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       <h3>{description}</h3>
     </Link>
     <p>
-      {amount} - {createdAt}
+      {numeral(amount / 100).format('$0,00.00')}
+      -
+      {moment(createdAt).format('Do MMMM YYYY')}
 
     </p>
     </Fragment>
