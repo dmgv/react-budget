766318db3a0bbdd46321a1eb8ad0aa519ad9903a
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-04-28 09:47:33 -0300

    Refactor EditExpensePage

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,21 +1,27 @@
 import React, { Fragment } from "react";
 import { connect } from "react-redux";
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from "../actions/filters";
-import { DateRangePicker } from 'react-dates'
+import {
+  setTextFilter,
+  sortByDate,
+  sortByAmount,
+  setStartDate,
+  setEndDate
+} from "../actions/filters";
+import { DateRangePicker } from "react-dates";
 
 class ExpenseListFilters extends React.Component {
   state = {
     calendarFocused: null
-  }
+  };
 
-  onDatesChange = ({startDate, endDate}) => {
-    this.props.dispatch(setStartDate(startDate))
-    this.props.dispatch(setEndDate(endDate))
-  }
+  onDatesChange = ({ startDate, endDate }) => {
+    this.props.dispatch(setStartDate(startDate));
+    this.props.dispatch(setEndDate(endDate));
+  };
 
-  onFocusChange = (calendarFocused) => {
-    this.setState(() => ({calendarFocused}))
-  }
+  onFocusChange = calendarFocused => {
+    this.setState(() => ({ calendarFocused }));
+  };
 
   render() {
     return (
@@ -24,17 +30,17 @@ class ExpenseListFilters extends React.Component {
           type="text"
           value={this.props.filters.text}
           onChange={e => {
-            props.dispatch(setTextFilter(e.target.value));
+            this.props.dispatch(setTextFilter(e.target.value));
           }}
         />
 
         <select
-          value={this.props.filters.sortyBy}
-          onChange={(e) => {
-            if(e.target.value === 'date') {
-              this.props.dispatch(sortByDate())
-            } else if (e.target.value === 'amount'){
-              this.props.dispatch(sortByAmount())
+          value={this.props.filters.sortBy}
+          onChange={e => {
+            if (e.target.value === "date") {
+              this.props.dispatch(sortByDate());
+            } else if (e.target.value === "amount") {
+              this.props.dispatch(sortByAmount());
             }
           }}
         >
@@ -53,10 +59,9 @@ class ExpenseListFilters extends React.Component {
           isOutsideRange={() => false}
         />
       </Fragment>
-    )
+    );
   }
-
-};
+}
 
 const mapStateToProps = state => {
   return {
