32f4bdd876aa44db70aec09a268c41a8d02c6c35
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-04-30 15:22:20 -0300

    Heroku Enviroment Variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,14 @@
 import * as firebase from "firebase";
 
+
+
 const config = {
-  apiKey: "AIzaSyDRtxgC0AUaZ_27OSxMsV1JDsUufCYi1Do",
-  authDomain: "expensify-38b0c.firebaseapp.com",
-  databaseURL: "https://expensify-38b0c.firebaseio.com",
-  projectId: "expensify-38b0c",
-  storageBucket: "expensify-38b0c.appspot.com",
-  messagingSenderId: "758335794295"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
