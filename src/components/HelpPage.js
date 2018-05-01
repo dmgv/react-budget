c2351bbafc059f22a01fc04ea0fc2fee89abecab
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-05-01 16:26:42 -0300

    Prettier

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
 
-const HelpPage = () => (<Fragment>This is from my help component</Fragment>);
+const HelpPage = () => <Fragment>This is from my help component</Fragment>;
 
 export default HelpPage;
