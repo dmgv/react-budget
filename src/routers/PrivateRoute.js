4e4af4560d7dd6de7dfa86de8af05b4853c93c6f
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-05-02 10:29:28 -0300

    Add private Routes

---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+import Header from "../components/Header";
+
+export const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    component={props =>
+      isAuthenticated ? (
+        <Fragment>
+          <Header /> <Component {...props} />
+        </Fragment>
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />
+);
+
+const mapStateToProps = state => ({
+  isAuthenticated: !!state.auth.uid
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
