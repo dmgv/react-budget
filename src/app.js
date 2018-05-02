c07bb354622cf425eeb2aca0c065d8aa0b9ab696
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-05-02 10:00:35 -0300

    Add auth reducer

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { startSetExpenses } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
+import { login, logout } from "./actions/auth";
 import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
@@ -12,15 +12,12 @@ import "react-dates/lib/css/_datepicker.css";
 import { firebase } from "./firebase/firebase";
 
 const store = configureStore();
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
 let hasRendered = false;
-
 const renderApp = () => {
   if (!hasRendered) {
     ReactDOM.render(jsx, document.getElementById("app"));
@@ -32,12 +29,15 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
-    store.dispatch(startSetExpenses()).then(() => {});
-    renderApp();
-    if (history.location.pathname === "/") {
-      history.push("dashboard");
-    }
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === "/") {
+        history.push("/dashboard");
+      }
+    });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push("/");
   }
